d5d2f1fc2238c8af011bbde56813d662c691db96
caio-henrique2006 <74103264+caio-henrique2006@users.noreply.github.com>
2022-01-15 20:52:49 -0300

    15/01/2022

--- index.js
+++ index.js
@@ -41,7 +41,53 @@ class greetings extends React.Component {
   }
 }*/
 
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date(),
+      name: "Caio Henrique"
+    };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+function App() {
+  return (
+    <div>
+      <Clock />
+      <Clock />
+      <Clock />
+    </div>
+  );
+}
+
 ReactDOM.render(
-	greet,
+	<App/>,
 	document.getElementById('root')
 );
